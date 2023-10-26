58fc0f43f5088bae8cb76403d5693d377b501236
OutlookBlo69420 <141130754+OutlookBlo69420@users.noreply.github.com>
2023-10-26 20:15:16 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+img="";
+status="";
+objects = [];
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+}
+
+function preload()
+{
+    img = loadImage("images.jpeg");
+    img = loadImage("images (1).jpeg");
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw()
+{
+ if(status!="")
+ {
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    objectDectector.detect(gotResult);
+for (i = 0; i < objects.length; i++)
+{
+    document.getElementById("status").innerHTML = "Status: Object Detected";
+    document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: " + objects.length;
+    fill(r,g,b);
+    percent = floor(objects[i].confidence * 100);
+    text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+    noFill();
+    stroke(r,g,b);
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+}}}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error)
+    }
+    console.log(results);
+    objects = results;
+}
